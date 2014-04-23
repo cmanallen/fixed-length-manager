e9775c29001b324152334454c3e1ee87a44eb7dc
cmanallen <cmanallen90@gmail.com>
2014-04-23 18:36:09 -0400

    added example import, added example py file, updated main import

--- space_seperated_import.py
+++ space_seperated_import.py
@@ -5,20 +5,25 @@ class SpaceSeperatedImport(object):
 		self.character_list = character_list
 
 	def convert_file_to_object(self):
-		rows = self.uploaded_file.split('\n')
-		row_array = {}
+		rows = self.uploaded_file.read().split('\n')
+		i = 0
+		row_object = {}
 
 		for row in rows:
-			row_array.append(self.split_row_into_columns(row, self.character_list))
+			row_object[i] = self._split_row_into_columns(row, self.character_list)
+			i += 1
 
-		return row_array
+		return row_object
 
-	def split_row_into_columns(self, string, column_count):
+	def _split_row_into_columns(self, string, column_count):
 		position = 0
-		column_array = {}
+		j = 0
+		column_object = {}
 
 		for column in column_count:
-			column_array.append(column[position:position+column])
+			end = position + column
+			column_object[j] = string[position:end:]
 			position += column
+			j += 1
 		
-		return column_array
+		return column_object
